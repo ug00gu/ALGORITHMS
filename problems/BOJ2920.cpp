1a31127218ebe826467f556b8742d7502e4d8029
jay <wjdgus392@ajou.ac.kr>
2017-11-17 15:34:39 +0900

    BOJ 2920

---
+++ problems/BOJ2920.cpp
@@ -0,0 +1,50 @@
+// source : https://www.acmicpc.net/problem/2920
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int a;
+	int i;
+	int num[8];
+
+	for(i=0; i<8;i++){
+		cin>>num[i];
+	}
+
+
+	switch(num[0]){
+		case 1:
+		a = 2;
+
+		for(i=1;i<8;i++){
+			if(a++ != num[i]){
+				cout<<"mixed";
+				return 0;
+			}
+		}
+		cout<<"ascending";
+		break;
+
+		case 8:
+		a = 7;
+
+		for(i=1;i<8;i++){
+			if(a-- != num[i]){
+				cout<<"mixed";
+				return 0;
+			}
+		}
+		cout<<"decending";
+		break;
+
+		default :
+		cout<<"mixed";
+		break;
+	}
+
+return 0;
+
+}
